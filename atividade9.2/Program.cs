03d27d9a32a328863761b9720dcd1774c9fa1b4c
samela1 <sameladejesus@gmail.com>
2019-06-14 22:13:19 -0300

    começa aqui o commit, agora sei que vai

---
+++ atividade9.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace atividade9._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, result;
+
+            Console.WriteLine("Digite o limete");
+            n = int.Parse(Console.ReadLine());
+
+            for (int contador = 0; contador <= n; contador++)
+            {
+                if (contador++ % 2 != 0) ;
+                
+
+                    Console.WriteLine("O contador está em: " + contador);
+                
+            }
+        }
+    }
+}
